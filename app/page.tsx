e0da973f367360bb331b17b554bfcd04d41a82ed
Arman Dezfuli <armandezfuli@proton.me>
2026-05-29 19:12:29 +0330

    feat: implement UI layout, global styles, and React Bits animations

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 
 const Page = () => {
     return (
-        <div>Welcome to Next.js 16!</div>
+        <section>
+            <h1 className="text-center">The Hub for Every Dev <br /> Event You Can&#39;t Miss</h1>
+            <p className="text-center mt-5">Hackathons, Meetups, and Conferences, All in One Place</p>
+        </section>
     )
 }
 export default Page
